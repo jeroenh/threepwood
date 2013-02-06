dae6aac95b54fcbd4a07a900f671db8dac892e50
Cosmin Dumitru <cosu@cosu.ro>
2013-02-06 16:23:56 +0100

    - added distribute version in requirements. mysql-python wants a higher version ..

--- threepwood/apps/collector/models.py
+++ threepwood/apps/collector/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.simplejson import loads
 from django.utils.timezone import now
-
+1
 def _generate_key():
     """
     @return: generates uuid
