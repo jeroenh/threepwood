b29ed9fd0f6dff4f52f77303dfa54e23d209164a
Jeroen van der Ham <vdham@uva.nl>
2013-02-24 12:26:13 +0100

    add an iterator over the query set with gc

--- threepwood/apps/reports/tasks.py
+++ threepwood/apps/reports/tasks.py
@@ -4,6 +4,7 @@ from celery import task
 import bulkwhois.cymru
 import pygeoip
 import os
+import gc
 
 @task()
 def add(x,y):
@@ -11,8 +12,7 @@ def add(x,y):
 
 @task()
 def fillPeerInfo():
-    newPeers = PeerRecord.objects.filter(peerinfo__isnull=True)
-    for p in newPeers:
+    for p in queryset_iterator(PeerRecord.objects.filter(peerinfo__isnull=True)):
         convertedIP = convert6to4(p.ip)
         if ":" in convertedIP:
             iptype = 6
@@ -22,11 +22,31 @@ def fillPeerInfo():
             newPeerInfo = PeerInfo.objects.get(ip=convertedIP)
         except ObjectDoesNotExist:
             newPeerInfo = PeerInfo(ip=convertedIP,iptype=iptype)
-        newPeerInfo.save()
+            newPeerInfo.save()
         p.peerinfo_id = newPeerInfo.id
         p.save()
 
+# Taken from: http://djangosnippets.org/snippets/1949/
+def queryset_iterator(queryset, chunksize=1000):
+    '''''
+    Iterate over a Django Queryset ordered by the primary key
 
+    This method loads a maximum of chunksize (default: 1000) rows in it's
+    memory at the same time while django normally would load all rows in it's
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+
+    Note that the implementation of the iterator does not support ordered query sets.
+    '''
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunksize]:
+            pk = row.pk
+            yield row
+        gc.collect()
+        
 def convert6to4(ip):
     if not ":" in ip:
         return ip
