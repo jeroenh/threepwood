a0e93010f9a9191d6cebd2621bbebaa0207a3c66
Cosmin Dumitru <cosu@cosu.ro>
2013-07-22 14:00:25 +0200

    added country percentage

--- utils/process.py
+++ utils/process.py
@@ -155,24 +155,25 @@ def get_torrent_stats(torrent, countries=None, asns=None):
 if __name__ == "__main__":
 
     #global tele2 as is AS1257, NL is 13127
-    asns = [31615, 9143, 3265, 6830, 31615, 5615, 49562, 286, 1134, 12469, 13127]
+    # asns = [31615, 9143, 3265, 6830, 31615, 5615, 49562, 286, 1134, 12469, 13127]
+    #
+    # stats = get_country_counts()
+    # f = open("new-countries.txt", 'w')
+    # for country in stats.keys():
+    #     f.write("%s: %s\n" % (country, stats[country]))
+    # f.close()
+    # print "-=" * 40
+    # for t in Torrent.objects.all()[6:]:
+    #     #stats = get_torrent_stats(t,countries=['NL'], asns=asns)
+    #     result = {'name': t.torrentmetadata.name, 'stats': stats}
+    #     #pprint.pprint(result)
+    #     sum = 0
+    #     sorted_stats = sorted(result['stats']['NL'], key=lambda k: k['name'])
+    #     #print(sorted_stats)
+    #     for k in sorted_stats:
+    #         print "{0:2.2f}".format(k['percent_of_country']),
+    #     print "\n"
+    # for k in sorted_stats:
+    #     print "\"{0}\"".format(k['name']),
 
-    stats = get_country_counts()
-    f = open("new-countries.txt", 'w')
-    for country in stats.keys():
-        f.write("%s: %s\n" % (country, stats[country]))
-    f.close()
-    print "-=" * 40
-    for t in Torrent.objects.all()[6:]:
-        #stats = get_torrent_stats(t,countries=['NL'], asns=asns)
-        result = {'name': t.torrentmetadata.name, 'stats': stats}
-        #pprint.pprint(result)
-        sum = 0
-        sorted_stats = sorted(result['stats']['NL'], key=lambda k: k['name'])
-        #print(sorted_stats)
-        for k in sorted_stats:
-            print "{0:2.2f}".format(k['percent_of_country']),
-        print "\n"
-    for k in sorted_stats:
-        print "\"{0}\"".format(k['name']),
-	
+    get_country_stats_new()
